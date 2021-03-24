a94a77e862c70589a1af700775cbd5a4a71b2d6c
Ishan Gawali <ishangawali02@gmail.com>
2021-03-24 17:38:27 +0530

    Product model COmpleted

---
+++ server/models/product.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+    product_id: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    content: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    images: {
+        type: Object,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    checked: {
+        type: Boolean,
+        default: false
+    },
+    sold: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true
+})
+
+mongoose.model("Product", productSchema)
