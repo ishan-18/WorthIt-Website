992b25ca91c7c4009f2d3d63c53b7c4e6e6ad5af
Ishan Gawali <ishangawali02@gmail.com>
2021-03-24 15:33:37 +0530

    Added Middleware for private routes

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const auth = (req,res,next) => {
+    try {
+        const token = req.header("Authorization")
+        if(!token) return res.status(401).json({msg: "Access Denied"})
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
+            if(err) return res.status(401).json({msg: "Register or Login required"})
+            req.user = user
+            next()
+        })
+    } catch (err) {
+        return res.status(500).json({err: err.message})
+    }
+}
+
+module.exports = auth
