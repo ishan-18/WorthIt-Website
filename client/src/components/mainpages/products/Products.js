8592e610cbd5d155770e9a76d014e73a3b510010
Ishan Gawali <ishangawali02@gmail.com>
2021-03-25 12:30:10 +0530

    Detail Page and Product Page Finished

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
 
 function Products() {
+    const state = useContext(GlobalState)
+    const [products] = state.productsAPI.products
+    console.log(products)
     return (
-        <div>
-            Products Component
+        <div className="products">
+            {
+                products.map(product=>{
+                    return <ProductItem key={product._id} product={product}/>
+                })
+            }
         </div>
     )
 }
